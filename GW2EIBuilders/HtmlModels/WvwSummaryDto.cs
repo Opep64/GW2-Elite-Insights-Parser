fa29ba10d4a40b80fd0d448bfb9d898c3a4cb211
Opep64 <166227755+Opep64@users.noreply.github.com>
2026-04-16 15:08:19 -0400

    Add WvW summary downstate analysis

--- GW2EIBuilders/HtmlModels/WvwSummaryDto.cs
+++ GW2EIBuilders/HtmlModels/WvwSummaryDto.cs
@@ -5,7 +5,9 @@ using GW2EIEvtcParser.LogLogic;
 using GW2EIEvtcParser.ParsedData;
 using System;
 using System.Globalization;
+using Segment = GW2EIEvtcParser.EIData.GenericSegment<double>;
 using static GW2EIEvtcParser.ParserHelper;
+using static GW2EIEvtcParser.SkillIDs;
 using static GW2EIEvtcParser.SpeciesIDs;
 
 namespace GW2EIBuilders.HtmlModels;
@@ -32,6 +34,12 @@ internal class WvwSummaryDto
     public List<WvwSummaryTopPlayerDto> TopEnemyDamagePlayers { get; set; } = [];
     public List<WvwSummaryTopPlayerDto> TopEnemyStripPlayers { get; set; } = [];
     public List<WvwSummaryTopPlayerDto> TopEnemyCrowdControlPlayers { get; set; } = [];
+    public List<WvwSummaryMetricRowDto> DownsOutcomeRows { get; set; } = [];
+    public List<WvwSummaryMetricRowDto> DownedStateRows { get; set; } = [];
+    public List<WvwSummaryDownDetailEntryDto> SquadMaximumVulnerabilityEntries { get; set; } = [];
+    public List<WvwSummaryDownDetailEntryDto> EnemyMaximumVulnerabilityEntries { get; set; } = [];
+    public List<WvwSummaryDownDetailEntryDto> SquadMaximumBurningEntries { get; set; } = [];
+    public List<WvwSummaryDownDetailEntryDto> EnemyMaximumBurningEntries { get; set; } = [];
 
     public static WvwSummaryDto? Build(ParsedEvtcLog log, PhaseData phase)
     {
@@ -53,6 +61,8 @@ internal class WvwSummaryDto
 
         var squad = BuildSide(log, phase, squadActors, hostileDamageTargets, hostilePlayerTargets, "My Squad");
         var enemy = BuildSide(log, phase, hostilePlayerTargets, squadActors, squadActors, "Enemy Team");
+        var squadDownState = BuildDownStateSide(log, phase, squadActors);
+        var enemyDownState = BuildDownStateSide(log, phase, hostilePlayerTargets);
 
         return new WvwSummaryDto
         {
@@ -67,6 +77,12 @@ internal class WvwSummaryDto
             Squad = squad,
             Enemy = enemy,
             MetricRows = BuildMetricRows(durationInMilliseconds, squad, enemy),
+            DownsOutcomeRows = BuildDownsOutcomeRows(squadDownState, enemyDownState),
+            DownedStateRows = BuildDownedStateRows(squadDownState, enemyDownState),
+            SquadMaximumVulnerabilityEntries = squadDownState.MaximumVulnerabilityEntries,
+            EnemyMaximumVulnerabilityEntries = enemyDownState.MaximumVulnerabilityEntries,
+            SquadMaximumBurningEntries = squadDownState.MaximumBurningEntries,
+            EnemyMaximumBurningEntries = enemyDownState.MaximumBurningEntries,
             TopDamagePlayers = BuildTopDamagePlayers(log, squadActors, hostilePlayerTargets, phase),
             TopStripPlayers = BuildTopStripPlayers(log, squadActors, phase),
             TopCleansePlayers = BuildTopCleansePlayers(log, squadActors, phase),
@@ -180,6 +196,236 @@ internal class WvwSummaryDto
             new WvwSummaryMetricRowDto("Deaths", squad.Deaths.ToString(), enemy.Deaths.ToString(), false, false, 0, false, false),
             new WvwSummaryMetricRowDto("Received CC", squad.ReceivedCrowdControl.ToString(), enemy.ReceivedCrowdControl.ToString(), false, false, 0, false, false),
         ];
+    }
+
+    private static List<WvwSummaryMetricRowDto> BuildDownsOutcomeRows(WvwSummaryDownStateSideDto squad, WvwSummaryDownStateSideDto enemy)
+    {
+        return
+        [
+            new WvwSummaryMetricRowDto("Downs", squad.Downs.ToString(), enemy.Downs.ToString()),
+            new WvwSummaryMetricRowDto("Kill Conversions", squad.KillConversions.ToString(), enemy.KillConversions.ToString()),
+            new WvwSummaryMetricRowDto("Downs Converted", FormatPercentage(squad.KillConversionRate, squad.Downs > 0), FormatPercentage(enemy.KillConversionRate, enemy.Downs > 0), false, false, 1, true),
+            new WvwSummaryMetricRowDto("Kill Time Avg", FormatDurationSeconds(squad.AverageKillTime), FormatDurationSeconds(enemy.AverageKillTime), false, false, 1, false, false),
+            new WvwSummaryMetricRowDto("Kill Time Min", FormatDurationSeconds(squad.MinimumKillTime), FormatDurationSeconds(enemy.MinimumKillTime), false, false, 1, false, false),
+            new WvwSummaryMetricRowDto("Kill Time Max", FormatDurationSeconds(squad.MaximumKillTime), FormatDurationSeconds(enemy.MaximumKillTime), false, false, 1, false, false),
+            new WvwSummaryMetricRowDto("Rezzes", squad.Rezzes.ToString(), enemy.Rezzes.ToString()),
+            new WvwSummaryMetricRowDto("Downs Rezzed", FormatPercentage(squad.RezRate, squad.Downs > 0), FormatPercentage(enemy.RezRate, enemy.Downs > 0), false, false, 1, true),
+            new WvwSummaryMetricRowDto("Rez Time Avg", FormatDurationSeconds(squad.AverageRezTime), FormatDurationSeconds(enemy.AverageRezTime), false, false, 1, false, false),
+            new WvwSummaryMetricRowDto("Rez Time Min", FormatDurationSeconds(squad.MinimumRezTime), FormatDurationSeconds(enemy.MinimumRezTime), false, false, 1, false, false),
+            new WvwSummaryMetricRowDto("Rez Time Max", FormatDurationSeconds(squad.MaximumRezTime), FormatDurationSeconds(enemy.MaximumRezTime), false, false, 1, false, false),
+        ];
+    }
+
+    private static List<WvwSummaryMetricRowDto> BuildDownedStateRows(WvwSummaryDownStateSideDto squad, WvwSummaryDownStateSideDto enemy)
+    {
+        return
+        [
+            new WvwSummaryMetricRowDto("Downed with Poison", FormatPercentage(squad.PoisonRate, squad.Downs > 0), FormatPercentage(enemy.PoisonRate, enemy.Downs > 0), false, false, 1, true),
+            new WvwSummaryMetricRowDto("Downed with Immobile", FormatPercentage(squad.ImmobileRate, squad.Downs > 0), FormatPercentage(enemy.ImmobileRate, enemy.Downs > 0), false, false, 1, true),
+            new WvwSummaryMetricRowDto("Downed with Vulnerability", FormatPercentage(squad.VulnerabilityRate, squad.Downs > 0), FormatPercentage(enemy.VulnerabilityRate, enemy.Downs > 0), false, false, 1, true),
+            new WvwSummaryMetricRowDto("Vulnerability Avg Stacks", FormatStacks(squad.AverageVulnerabilityStacksOnAffectedDowns), FormatStacks(enemy.AverageVulnerabilityStacksOnAffectedDowns), false, false, 1),
+            new WvwSummaryMetricRowDto("Vulnerability Min Stacks", FormatStackRangeValue(squad.MinimumVulnerabilityStacks, squad.Downs > 0), FormatStackRangeValue(enemy.MinimumVulnerabilityStacks, enemy.Downs > 0), false, false, 0, false, false),
+            new WvwSummaryMetricRowDto("Vulnerability Max Stacks", FormatStackRangeValue(squad.MaximumVulnerabilityStacks, squad.Downs > 0), FormatStackRangeValue(enemy.MaximumVulnerabilityStacks, enemy.Downs > 0), false, false, 0, false, false),
+            new WvwSummaryMetricRowDto("Downed with Burning", FormatPercentage(squad.BurningRate, squad.Downs > 0), FormatPercentage(enemy.BurningRate, enemy.Downs > 0), false, false, 1, true),
+            new WvwSummaryMetricRowDto("Burning Avg Stacks", FormatStacks(squad.AverageBurningStacksOnAffectedDowns), FormatStacks(enemy.AverageBurningStacksOnAffectedDowns), false, false, 1),
+            new WvwSummaryMetricRowDto("Burning Min Stacks", FormatStackRangeValue(squad.MinimumBurningStacks, squad.Downs > 0), FormatStackRangeValue(enemy.MinimumBurningStacks, enemy.Downs > 0), false, false, 0, false, false),
+            new WvwSummaryMetricRowDto("Burning Max Stacks", FormatStackRangeValue(squad.MaximumBurningStacks, squad.Downs > 0), FormatStackRangeValue(enemy.MaximumBurningStacks, enemy.Downs > 0), false, false, 0, false, false),
+        ];
+    }
+
+    private static WvwSummaryDownStateSideDto BuildDownStateSide(ParsedEvtcLog log, PhaseData phase, IReadOnlyList<SingleActor> actors)
+    {
+        var result = new WvwSummaryDownStateSideDto();
+        int downsWithVulnerability = 0;
+        long totalVulnerabilityStacksOnAffectedDowns = 0;
+        int? minimumVulnerabilityStacks = null;
+        int? maximumVulnerabilityStacks = null;
+        int downsWithBurning = 0;
+        long totalBurningStacksOnAffectedDowns = 0;
+        int? minimumBurningStacks = null;
+        int? maximumBurningStacks = null;
+        foreach (SingleActor actor in actors)
+        {
+            var (_, downs, _, _) = actor.GetStatus(log);
+            IReadOnlyList<AliveEvent> aliveEvents = log.CombatData.GetAliveEvents(actor.AgentItem);
+            IReadOnlyList<DeadEvent> deadEvents = log.CombatData.GetDeadEvents(actor.AgentItem);
+
+            foreach (Segment down in downs)
+            {
+                if (down.Start < phase.Start || down.Start > phase.End)
+                {
+                    continue;
+                }
+
+                result.Downs++;
+                long buffCheckTime = Math.Max(log.LogData.LogStart, down.Start - ServerDelayConstant);
+
+                int poisonStacks = GetBuffStacksAtTime(actor, log, Poison, buffCheckTime);
+                if (poisonStacks > 0)
+                {
+                    result.DownsWithPoison++;
+                }
+
+                int immobileStacks = GetBuffStacksAtTime(actor, log, Immobile, buffCheckTime);
+                if (immobileStacks > 0)
+                {
+                    result.DownsWithImmobile++;
+                }
+
+                int vulnerabilityStacks = GetBuffStacksAtTime(actor, log, Vulnerability, buffCheckTime);
+                UpdateStackStats(vulnerabilityStacks, ref downsWithVulnerability, ref totalVulnerabilityStacksOnAffectedDowns, ref minimumVulnerabilityStacks, ref maximumVulnerabilityStacks);
+                UpdateMaximumEntries(
+                    result.MaximumVulnerabilityEntries,
+                    vulnerabilityStacks,
+                    maximumVulnerabilityStacks,
+                    actor,
+                    down.Start,
+                    phase.Start);
+
+                int burningStacks = GetBuffStacksAtTime(actor, log, Burning, buffCheckTime);
+                UpdateStackStats(burningStacks, ref downsWithBurning, ref totalBurningStacksOnAffectedDowns, ref minimumBurningStacks, ref maximumBurningStacks);
+                UpdateMaximumEntries(
+                    result.MaximumBurningEntries,
+                    burningStacks,
+                    maximumBurningStacks,
+                    actor,
+                    down.Start,
+                    phase.Start);
+
+                if (down.End > phase.End)
+                {
+                    continue;
+                }
+
+                double downDurationSeconds = Math.Max(0, down.End - down.Start) / 1000.0;
+                if (HasStatusEventAtTime(deadEvents, down.End))
+                {
+                    result.KillConversions++;
+                    result.KillTimes.Add(downDurationSeconds);
+                }
+                else if (HasStatusEventAtTime(aliveEvents, down.End))
+                {
+                    result.Rezzes++;
+                    result.RezTimes.Add(downDurationSeconds);
+                }
+            }
+        }
+
+        result.DownsWithVulnerability = downsWithVulnerability;
+        result.TotalVulnerabilityStacksOnAffectedDowns = totalVulnerabilityStacksOnAffectedDowns;
+        result.MinimumVulnerabilityStacks = minimumVulnerabilityStacks;
+        result.MaximumVulnerabilityStacks = maximumVulnerabilityStacks;
+        result.DownsWithBurning = downsWithBurning;
+        result.TotalBurningStacksOnAffectedDowns = totalBurningStacksOnAffectedDowns;
+        result.MinimumBurningStacks = minimumBurningStacks;
+        result.MaximumBurningStacks = maximumBurningStacks;
+        result.KillConversionRate = result.Downs > 0 ? Math.Round(100.0 * result.KillConversions / result.Downs, 1) : 0.0;
+        result.RezRate = result.Downs > 0 ? Math.Round(100.0 * result.Rezzes / result.Downs, 1) : 0.0;
+        result.PoisonRate = result.Downs > 0 ? Math.Round(100.0 * result.DownsWithPoison / result.Downs, 1) : 0.0;
+        result.ImmobileRate = result.Downs > 0 ? Math.Round(100.0 * result.DownsWithImmobile / result.Downs, 1) : 0.0;
+        result.VulnerabilityRate = result.Downs > 0 ? Math.Round(100.0 * result.DownsWithVulnerability / result.Downs, 1) : 0.0;
+        result.BurningRate = result.Downs > 0 ? Math.Round(100.0 * result.DownsWithBurning / result.Downs, 1) : 0.0;
+        result.AverageKillTime = GetAverage(result.KillTimes);
+        result.MinimumKillTime = GetMinimum(result.KillTimes);
+        result.MaximumKillTime = GetMaximum(result.KillTimes);
+        result.AverageRezTime = GetAverage(result.RezTimes);
+        result.MinimumRezTime = GetMinimum(result.RezTimes);
+        result.MaximumRezTime = GetMaximum(result.RezTimes);
+        result.AverageVulnerabilityStacksOnAffectedDowns = result.DownsWithVulnerability > 0 ? Math.Round((double)result.TotalVulnerabilityStacksOnAffectedDowns / result.DownsWithVulnerability, 1) : null;
+        result.AverageBurningStacksOnAffectedDowns = result.DownsWithBurning > 0 ? Math.Round((double)result.TotalBurningStacksOnAffectedDowns / result.DownsWithBurning, 1) : null;
+
+        return result;
+    }
+
+    private static void UpdateStackStats(int stacks, ref int affectedDowns, ref long totalStacksOnAffectedDowns, ref int? minimumStacks, ref int? maximumStacks)
+    {
+        if (stacks > 0)
+        {
+            affectedDowns++;
+            totalStacksOnAffectedDowns += stacks;
+        }
+
+        minimumStacks = minimumStacks.HasValue ? Math.Min(minimumStacks.Value, stacks) : stacks;
+        maximumStacks = maximumStacks.HasValue ? Math.Max(maximumStacks.Value, stacks) : stacks;
+    }
+
+    private static void UpdateMaximumEntries(
+        List<WvwSummaryDownDetailEntryDto> entries,
+        int stacks,
+        int? currentMaximum,
+        SingleActor actor,
+        long downTime,
+        long phaseStart)
+    {
+        if (!currentMaximum.HasValue || stacks <= 0)
+        {
+            return;
+        }
+
+        if (entries.Count == 0 || stacks > entries[0].StackCount)
+        {
+            entries.Clear();
+        }
+
+        if (stacks == currentMaximum.Value)
+        {
+            entries.Add(new WvwSummaryDownDetailEntryDto
+            {
+                Name = actor.Character,
+                Account = actor.Account,
+                Profession = actor.Spec.ToString(),
+                Icon = actor.GetIcon(),
+                UniqueId = actor.UniqueID,
+                StackCount = stacks,
+                Time = downTime,
+                TimeLabel = ToDurationString(Math.Max(0, downTime - phaseStart)),
+            });
+        }
+    }
+
+    private static int GetBuffStacksAtTime(SingleActor actor, ParsedEvtcLog log, long buffId, long time)
+    {
+        return (int)Math.Max(0, Math.Round(actor.GetBuffStatus(log, buffId, time).Value));
+    }
+
+    private static bool HasStatusEventAtTime<T>(IReadOnlyList<T> statusEvents, long time) where T : StatusEvent
+    {
+        return statusEvents.Any(evt => Math.Abs(evt.Time - time) <= ServerDelayConstant);
+    }
+
+    private static double? GetAverage(IReadOnlyList<double> values)
+    {
+        return values.Count > 0 ? Math.Round(values.Average(), 1) : null;
+    }
+
+    private static double? GetMinimum(IReadOnlyList<double> values)
+    {
+        return values.Count > 0 ? Math.Round(values.Min(), 1) : null;
+    }
+
+    private static double? GetMaximum(IReadOnlyList<double> values)
+    {
+        return values.Count > 0 ? Math.Round(values.Max(), 1) : null;
+    }
+
+    private static string FormatPercentage(double value, bool hasDenominator)
+    {
+        return hasDenominator ? value.ToString(CultureInfo.InvariantCulture) : "N/A";
+    }
+
+    private static string FormatDurationSeconds(double? value)
+    {
+        return value.HasValue ? value.Value.ToString(CultureInfo.InvariantCulture) : "N/A";
+    }
+
+    private static string FormatStacks(double? value)
+    {
+        return value.HasValue ? value.Value.ToString(CultureInfo.InvariantCulture) : "N/A";
+    }
+
+    private static string FormatStackRangeValue(int? value, bool hasDowns)
+    {
+        return hasDowns && value.HasValue ? value.Value.ToString() : "N/A";
     }
 
     private static List<WvwSummaryTopPlayerDto> BuildTopDamagePlayers(ParsedEvtcLog log, IReadOnlyList<SingleActor> squadActors, IReadOnlyList<SingleActor> hostilePlayerTargets, PhaseData phase)
@@ -475,4 +721,51 @@ internal class WvwSummaryCrowdControlEventInfo
 {
     public CrowdControlEvent Event { get; set; } = null!;
     public bool Effective { get; set; }
+}
+
+internal class WvwSummaryDownStateSideDto
+{
+    public int Downs { get; set; }
+    public int KillConversions { get; set; }
+    public int Rezzes { get; set; }
+    public int DownsWithPoison { get; set; }
+    public int DownsWithImmobile { get; set; }
+    public int DownsWithVulnerability { get; set; }
+    public int DownsWithBurning { get; set; }
+    public long TotalVulnerabilityStacksOnAffectedDowns { get; set; }
+    public long TotalBurningStacksOnAffectedDowns { get; set; }
+    public int? MinimumVulnerabilityStacks { get; set; }
+    public int? MaximumVulnerabilityStacks { get; set; }
+    public int? MinimumBurningStacks { get; set; }
+    public int? MaximumBurningStacks { get; set; }
+    public double KillConversionRate { get; set; }
+    public double RezRate { get; set; }
+    public double PoisonRate { get; set; }
+    public double ImmobileRate { get; set; }
+    public double VulnerabilityRate { get; set; }
+    public double BurningRate { get; set; }
+    public double? AverageKillTime { get; set; }
+    public double? MinimumKillTime { get; set; }
+    public double? MaximumKillTime { get; set; }
+    public double? AverageRezTime { get; set; }
+    public double? MinimumRezTime { get; set; }
+    public double? MaximumRezTime { get; set; }
+    public double? AverageVulnerabilityStacksOnAffectedDowns { get; set; }
+    public double? AverageBurningStacksOnAffectedDowns { get; set; }
+    public List<double> KillTimes { get; } = [];
+    public List<double> RezTimes { get; } = [];
+    public List<WvwSummaryDownDetailEntryDto> MaximumVulnerabilityEntries { get; } = [];
+    public List<WvwSummaryDownDetailEntryDto> MaximumBurningEntries { get; } = [];
+}
+
+internal class WvwSummaryDownDetailEntryDto
+{
+    public string Name { get; set; } = "";
+    public string Account { get; set; } = "";
+    public string Profession { get; set; } = "";
+    public string Icon { get; set; } = "";
+    public int UniqueId { get; set; }
+    public int StackCount { get; set; }
+    public long Time { get; set; }
+    public string TimeLabel { get; set; } = "";
 }
